a19bb1de1ef5da3dee881d29f96ebe610fb59eef
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2023-12-18 13:39:15 +0100

    completed basic functionality

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -1,20 +1,32 @@
 let CountDown = ()=> {
     let currentDate = new Date().getTime()
     let launchDate = new Date('December 31, 2023 23:59:59').getTime()
+    let timeTillLaunch = launchDate-currentDate
+    console.log(timeTillLaunch);
+    let launchTimeInfo ={};
+    if (timeTillLaunch>0) {
+        launchTimeInfo = {
+            staticLaunchDate: launchDate,
+            day: Math.floor(timeTillLaunch/(1000*60*60*24)),
+            hour: Math.floor((timeTillLaunch%(1000*60*60*24))/(1000*60*60)),
+            minutes: Math.floor((timeTillLaunch%(1000*60*60)/(1000*60))),
+            seconds: Math.floor((timeTillLaunch%(1000*60))/1000),
+        }
+    }
     // let dateRef = (param)=>   new Date(param);
     // let staticDateRefRef = dateRef('01'+"/"+'01'+"/"+'2024')
-    let launchTimeInfo = {
-        staticLaunchDate: "dateRef('01/01/2024').toLocaleDateString()",
-        day: "staticDateRefRef.getUTCDate()",
-        hour: "staticDateRefRef.getUTCHours()",
-        minutes: "staticDateRefRef.getMinutes()",
-        seconds: "staticDateRefRef.getUTCSeconds()",
-        // day: staticDateRefRef.getUTCDate(),
-        // hour: staticDateRefRef.getUTCHours(),
-        // minutes: staticDateRefRef.getMinutes(),
-        // seconds: staticDateRefRef.getUTCSeconds()
+    // let launchTimeInfo = {
+    //     staticLaunchDate: "dateRef('01/01/2024').toLocaleDateString()",
+    //     day: "staticDateRefRef.getUTCDate()",
+    //     hour: "staticDateRefRef.getUTCHours()",
+    //     minutes: "staticDateRefRef.getMinutes()",
+    //     seconds: "staticDateRefRef.getUTCSeconds()",
+    //     // day: staticDateRefRef.getUTCDate(),
+    //     // hour: staticDateRefRef.getUTCHours(),
+    //     // minutes: staticDateRefRef.getMinutes(),
+    //     // seconds: staticDateRefRef.getUTCSeconds()
 
-    }
+    // }
     return (
         <>
         <div className="mb-10 text-center">
