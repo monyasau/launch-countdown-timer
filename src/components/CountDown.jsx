5a2c7e6a27059e8733c9dbbe7750a8ac1124e4e5
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-04 14:26:26 +0100

    update: Added a countdown expiration fix

--- src/components/CountDown.jsx
+++ src/components/CountDown.jsx
@@ -2,8 +2,9 @@ import { useEffect , useState} from "react"
 
 let CountDown = ()=> {
     let currentDate = new Date().getTime()
-    let launchDate = new Date(`December 31, ${new Date().getFullYear()-1} 23:59:59`).getTime()
+    let launchDate = new Date(`December 31, ${new Date().getFullYear()} 23:59:59`).getTime()
     const [timeTillLaunch, setTimeTillLaunch] = useState(launchDate-currentDate)
+    const [expired, setExpired] = useState(false)
     const [launchTimeInfo, setLaunchTimeInfo] = useState({
         staticLaunchDate: new Date(launchDate+86400000).toLocaleDateString(),
         day: Math.floor(timeTillLaunch/(1000*60*60*24)),
@@ -22,6 +23,8 @@ let CountDown = ()=> {
             seconds: Math.floor((timeTillLaunch%(1000*60))/1000),
         })
         setTimeTillLaunch(launchDate-currentDate)
+    }else{
+        setExpired(true)
     }
         }, 1000);
     
@@ -31,6 +34,8 @@ let CountDown = ()=> {
         <>
         <div className="h-[75%] flex items-center">
         <div className=" text-center  w-full">
+{(expired===false)?
+<>
         <div className="mt-12 md:mt-0" aria-label="header">
             <h3 className="text-center font-bold md:tracking-[10px] tracking-wider">
                 WE'RE LAUNCHING SOON
@@ -54,8 +59,37 @@ let CountDown = ()=> {
                 Seconds
                 </div>
             </div>
+        <div className="text-center">Launch date will be: {launchTimeInfo.staticLaunchDate}</div>
+        </>
+        :
+        <>
+                <div className="mt-12 md:mt-0" aria-label="header">
+            <h3 className="text-center font-bold md:tracking-[10px] tracking-wider">
+                WE'RE HAVE LAUNCHED
+            </h3>
+        </div>
+            <div className="grid md:grid-cols-4 grid-cols-2 md:my-20 my-10 md:w-[70%] mx-auto">
+            <div className="flex items-center flex-col ">
+            <div className="rounded bg-dark-gray w-[70%] flex shadow-xl justify-center items-center h-[150px] text-white text-7xl"><h1>-</h1></div>
+                Days
+            </div>
+            <div className="flex items-center flex-col ">
+            <div className="rounded bg-dark-gray w-[70%] flex shadow-xl justify-center items-center h-[150px] text-white text-7xl"><h1>-</h1></div>
+            Hours
+            </div>
+            <div className="flex items-center flex-col ">
+            <div className="rounded bg-dark-gray w-[70%] flex shadow-xl justify-center items-center h-[150px] text-white text-7xl"><h1>-</h1></div>
+            Minutes
+            </div>
+            <div className="flex items-center flex-col ">
+            <div className="rounded bg-dark-gray w-[70%] flex shadow-xl justify-center items-center h-[150px] text-white text-7xl"><h1>-</h1></div>
+            Seconds
+            </div>
+        </div>
             
-        <div className="text-center">Launch date: {launchTimeInfo.staticLaunchDate}</div>
+        <div className="text-center">Launch date was: {launchTimeInfo.staticLaunchDate}</div>
+        </>
+}
             </div>
             </div>
             </>
